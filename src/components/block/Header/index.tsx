12015a0d6d60de2dfa7038fd3242e5408919398e
Rodrigo Russo <rodrigo.esteves@webjump.com.br>
2021-03-10 11:02:41 -0300

    Change Header to styled components

--- src/components/block/Header/index.tsx
+++ src/components/block/Header/index.tsx
@@ -1,27 +1,3 @@
-import {
-    HeaderElement,
-    HeaderNavigation,
-    NavLink,
-    HeaderSubnav,
-    AppBrand,
-    AppTitle
-} from './styles';
-import {ReactComponent as Logo} from 'assets/Logo-white.svg';
-
-const Header = () => {
-    return (
-        <HeaderElement>
-            <HeaderNavigation order={1}>
-                <NavLink to="/filmes">Filmes</NavLink>
-                <NavLink to="/series">Series</NavLink>
-            </HeaderNavigation>
-            <AppBrand href="/" order={2}>
-                <Logo />
-                <AppTitle>Cinejump!</AppTitle>
-            </AppBrand>
-            <HeaderSubnav order={3} />
-        </HeaderElement>
-    );
-}
+import { Header } from './Header';
 
 export default Header;
--- src/components/block/Header/styles.tsx
+++ src/components/block/Header/styles.tsx
@@ -1,5 +1,7 @@
 import styled from 'styled-components';
 import { Link as RouterLink } from 'react-router-dom';
+import { Breakpoints } from 'enums/style';
+import { ReactComponent as Logo } from 'assets/Logo-white.svg';
 
 interface HeaderChildProps {
     order: number
@@ -13,6 +15,13 @@ export const HeaderElement = styled.header`
 
 export const AppBrand = styled.a<HeaderChildProps>`
     order: ${props => props.order};
+    width: 35vw;
+    max-width: 150px;
+`;
+
+export const AppLogo = styled(Logo)`
+    width: 100%;
+    height: auto;
 `;
 
 export const AppTitle = styled.h1`
@@ -30,17 +39,26 @@ export const AppTitle = styled.h1`
 export const NavLink = styled(RouterLink)`
     color: inherit;
     text-decoration: none;
-    margin-right: 10px;
+    font-size: 1.5rem;
+    margin-right: 15px;
 `;
 
 export const HeaderNavigation = styled.nav<HeaderChildProps>`
     flex: 1;
     order: ${props => props.order};
     color: white;
+    display: flex;
+    flex-direction: column;
+
+    @media screen and (min-width: ${Breakpoints.TABLET}) {
+        flex-direction: row;
+    }
 `;
 
 export const HeaderSubnav = styled.div<HeaderChildProps>`
 flex: 1;
 order: ${props => props.order};
+display: flex;
+justify-content: flex-end;
 `;
 

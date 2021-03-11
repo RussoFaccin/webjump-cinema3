e6fd9eec5e10e1067cac319e7ef017a88db829c5
Rodrigo Russo <rodrigo.esteves@webjump.com.br>
2021-03-11 14:09:55 -0300

    Return to previous state

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,17 +1,7 @@
 import React from 'react';
-import { Movie } from 'models/Movie';
-import MovieCard from 'components/element/movie-card';
 
 const Home = () => {
-    const movie = new Movie({
-        id: 527774,
-        title: '"Raya e o Último Dragão"',
-        poster_path: '/yXrb84zJidCefyhTWHwo4yCDvwz.jpg',
-        backdrop_path: '/7prYzufdIOy1KCTZKVWpjBFqqNr.jpg',
-
-    });
-    
-    return <MovieCard {...movie} />
+    return <h1>Home Page!</h1>
 };
 
 export default Home;

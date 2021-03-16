b5603966204db3037850f03b0bdd9aea40187394
Rodrigo Russo <rodrigo.esteves@webjump.com.br>
2021-03-16 17:43:43 -0300

    Fix component structure

--- src/components/element/MovieList/index.tsx
+++ src/components/element/MovieList/index.tsx
@@ -2,18 +2,24 @@ import React from "react";
 import { Movie } from "models/Movie";
 import MovieCard from "components/element/movie-card";
 import ScrollDrag from 'components/element/scroll-drag';
+import { Container, Heading } from './styles';
 
 interface MovieListProps {
   title: string;
   movies: Movie[];
 }
 
-export const MovieList = ({ title, movies }: MovieListProps) => {
+const MovieList = ({ title, movies }: MovieListProps) => {
   const tmpList = movies.map((movie, index) => {
     return <MovieCard key={`${movie.id}_${index}`} {...movie} />;
   });
 
   return (
-    <ScrollDrag>{tmpList}</ScrollDrag>
+    <Container>
+      <Heading>{title}</Heading>
+      <ScrollDrag>{tmpList}</ScrollDrag>
+    </Container>
   );
 };
+
+export default MovieList;

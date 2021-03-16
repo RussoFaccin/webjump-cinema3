258db58d35b8f5ea8431c412a99d38426d3c2660
Rodrigo Russo <rodrigo.esteves@webjump.com.br>
2021-03-16 13:30:25 -0300

    [Home] Base structure

--- src/components/block/Header/Header.tsx
+++ src/components/block/Header/Header.tsx
@@ -1,35 +1,38 @@
 import {
   HeaderElement,
+  HeaderContent,
   HeaderNavigation,
   NavLink,
   HeaderSubnav,
   AppBrand,
   AppLogo,
-  AppTitle
-} from './styles';
-import CircleButton from 'components/element/circle-button';
+  AppTitle,
+} from "./styles";
+import CircleButton from "components/element/circle-button";
 import { CgProfile } from "react-icons/cg";
 import { BiSearch } from "react-icons/bi";
 
 export const Header = () => {
   return (
-      <HeaderElement>
-          <HeaderNavigation order={1}>
-              <NavLink to="/filmes">Filmes</NavLink>
-              <NavLink to="/series">Series</NavLink>
-          </HeaderNavigation>
-          <AppBrand href="/" order={2}>
-              <AppLogo />
-              <AppTitle>Cinejump!</AppTitle>
-          </AppBrand>
-          <HeaderSubnav order={3}>
-            <CircleButton text="Search">
-              <BiSearch size="70%" color="white" />
-            </CircleButton>
-            <CircleButton text="Profile">
-              <CgProfile size="70%" color="white" />
-            </CircleButton>
-          </HeaderSubnav>
-      </HeaderElement>
+    <HeaderElement>
+      <HeaderContent>
+        <HeaderNavigation order={1}>
+          <NavLink to="/filmes">Filmes</NavLink>
+          <NavLink to="/series">Series</NavLink>
+        </HeaderNavigation>
+        <AppBrand href="/" order={2}>
+          <AppLogo />
+          <AppTitle>Cinejump!</AppTitle>
+        </AppBrand>
+        <HeaderSubnav order={3}>
+          <CircleButton text="Search">
+            <BiSearch size="70%" color="white" />
+          </CircleButton>
+          <CircleButton text="Profile">
+            <CgProfile size="70%" color="white" />
+          </CircleButton>
+        </HeaderSubnav>
+      </HeaderContent>
+    </HeaderElement>
   );
-}
+};
---
+++ src/components/block/index.ts
@@ -0,0 +1,5 @@
+import Header from "components/block/Header";
+import Highlights from "components/block/Highlights";
+import Footer from "components/block/Footer";
+
+export { Header, Highlights, Footer };

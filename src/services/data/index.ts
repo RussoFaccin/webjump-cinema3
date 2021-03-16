165be7b7d538051423da40b8307704023e069bab
Rodrigo Russo <rodrigo.esteves@webjump.com.br>
2021-03-16 17:29:01 -0300

    Criar serviço para filmes populares

--- src/services/data/index.ts
+++ src/services/data/index.ts
@@ -1,3 +1,29 @@
-import { DataService } from "./data";
+import { api, API_CONFIG } from "services/api";
+import { Movie } from "models";
 
-export { DataService };
+export class DataService {
+  static async getMovieList(urlKey: string) {
+    const movieList = await api.get(`/${urlKey}`, API_CONFIG);
+
+    return movieList.data.results;
+  }
+
+  static formatDataAPI(dataEntry: Movie[], qtyLimit = 0): Movie[] {
+    enum ImageSizes {
+      BACKDROP = "w780",
+      POSTER = "w342",
+    }
+
+    const BACKDROP_URL = `https://image.tmdb.org/t/p/${ImageSizes.BACKDROP}`;
+    const POSTER_URL = `https://image.tmdb.org/t/p/${ImageSizes.POSTER}`;
+
+    const tmpList = dataEntry.map((movie: Movie) => {
+      movie.backdrop_path = `${BACKDROP_URL}${movie.backdrop_path}`;
+      movie.poster_path = `${POSTER_URL}${movie.poster_path}`;
+
+      return movie;
+    });
+
+    return qtyLimit === 0 ? dataEntry : tmpList.slice(0, qtyLimit);
+  }
+}

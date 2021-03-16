1bf1391a379f73f37197d76366226b0c50b3805c
Rodrigo Russo <rodrigo.esteves@webjump.com.br>
2021-03-16 17:51:37 -0300

    Fix component imports

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,9 +1,9 @@
 import React, { useCallback, useEffect, useState } from "react";
 import { Header, Footer } from "components/block";
 import { AppContent } from "./styles";
-import MovieList from 'components/element/movie-list';
-import { DataService } from 'services/data';
-import { Movie } from 'models/';
+import { MovieList } from "components/element";
+import { DataService } from "services/data";
+import { Movie } from "models/";
 
 const Home = () => {
   const [popularMovies, setPopular] = useState<Movie[]>([]);
@@ -11,9 +11,9 @@ const Home = () => {
   useEffect(() => {
     getPopularMovies();
   });
-  
-  const getPopularMovies = useCallback(async() => {
-    const result = await DataService.getMovieList('popular');
+
+  const getPopularMovies = useCallback(async () => {
+    const result = await DataService.getMovieList("popular");
     setPopular(DataService.formatDataAPI(result));
   }, []);
 

18f54ad10025b432a8aabfd728f6ca0fcc5148d3
Rodrigo Russo <rodrigo.esteves@webjump.com.br>
2021-03-16 11:15:37 -0300

    Add ScrollContainer to MovieList

--- src/components/element/movie-list/MovieList.tsx
+++ src/components/element/movie-list/MovieList.tsx
@@ -1,7 +1,7 @@
-import React, {useRef, useState} from "react";
+import React from "react";
 import { Movie } from "models/Movie";
 import MovieCard from "components/element/movie-card";
-import { Container, Heading, ListContainer } from "./styles";
+import ScrollDrag from 'components/element/scroll-drag';
 
 interface MovieListProps {
   title: string;
@@ -9,63 +9,11 @@ interface MovieListProps {
 }
 
 export const MovieList = ({ title, movies }: MovieListProps) => {
-  const [state, setState] = useState({
-    isDragging: false,
-    left: 0,
-    posX: 0
+  const tmpList = movies.map((movie, index) => {
+    return <MovieCard key={`movie.id_${index}`} {...movie} />;
   });
-
-  const scrollContainer = useRef<HTMLDivElement>(null);
-
-  const tmpList = movies.map((movie) => {
-    return <MovieCard key={movie.id} {...movie} />;
-  });
-
-  function handleDragStart(evt: any) {
-    evt.preventDefault();
-  }
-
-  function handleMouseDown(evt: any) {
-    const left = scrollContainer.current ? scrollContainer.current.scrollLeft : state.left;
-    const posX = evt.clientX ? evt.clientX : evt.touches[0].clientX;
-
-    setState({
-      ...state,
-      isDragging: true,
-      left,
-      posX
-    });
-  }
-
-  function handleMouseMove(evt: any) {
-    if (!state.isDragging) {
-      return false;
-    }
-
-    const deltaX = evt.clientX ? evt.clientX - state.posX : evt.touches[0].clientX - state.posX;
-    
-    if (scrollContainer.current) {
-      scrollContainer.current.scrollLeft = state.left - deltaX;
-    }
-  }
-
-  function handleMouseUp(evt: any) {
-    setState({
-      ...state,
-      isDragging: false
-    });
-  }
 
   return (
-    <Container
-    ref={scrollContainer}
-      onDragStart={handleDragStart}
-      onMouseDown={handleMouseDown} onTouchStart={handleMouseDown}
-      onMouseMove={handleMouseMove} onTouchMove={handleMouseMove}
-      onMouseUp={handleMouseUp} onTouchEnd={handleMouseUp}
-    >
-      <Heading>{title}</Heading>
-      <ListContainer>{tmpList}</ListContainer>
-    </Container>
+    <ScrollDrag>{tmpList}</ScrollDrag>
   );
 };

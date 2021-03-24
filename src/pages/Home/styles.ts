b54e1d35eac2865e0c917675a44f69d4f1be7d08
Rodrigo Russo <rodrigo.esteves@webjump.com.br>
2021-03-24 14:51:27 -0300

    Remove index.css

--- src/pages/Home/styles.ts
+++ src/pages/Home/styles.ts
@@ -1,12 +1,12 @@
-import styled from 'styled-components';
-import { Breakpoints } from 'enums/style';
+import styled from "styled-components";
+import { Breakpoints } from "enums";
 
 export const AppContent = styled.main`
-    max-width: 1000px;
-    padding: 0 20px;
-    margin: auto;
+  max-width: 1000px;
+  padding: 0 20px;
+  margin: auto;
 
-    @media screen and (min-width: ${Breakpoints.FULL}) {
-        padding: 0;
-    }
+  @media screen and (min-width: ${Breakpoints.FULL}) {
+    padding: 0;
+  }
 `;

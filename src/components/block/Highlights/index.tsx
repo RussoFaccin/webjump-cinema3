45033208aca160557ed4b5a470584bea2ecf5498
Rodrigo Russo <brweb0117@ambev.com.br>
2021-04-19 09:41:43 -0300

    [Highlights] Fix movies display

--- src/components/block/Highlights/index.tsx
+++ src/components/block/Highlights/index.tsx
@@ -19,22 +19,22 @@ const Highlights = ({ movieList }: Props) => {
   return (
     <Container>
       <CardMain>
-        <CardPicture src={movieList[0].backdrop_path} alt="ASD" />
+        <CardPicture src={movieList[0].backdrop_path} alt={movieList[0].title} />
         <Description>
           <Title>{movieList[0].title}</Title>
           <Text>{movieList[0].overview}</Text>
         </Description>
       </CardMain>
       <CardSecondary>
-        <CardPicture src={movieList[1].poster_path} alt="ASD" />
+        <CardPicture src={movieList[1].backdrop_path} alt={movieList[1].title} />
         <Description>
           <Title>{movieList[1].title}</Title>
         </Description>
       </CardSecondary>
       <CardTertiary>
         <CardPicture
-          src="https://image.tmdb.org/t/p/w1280/hJuDvwzS0SPlsE6MNFOpznQltDZ.jpg"
-          alt="ASD"
+          src={movieList[2].backdrop_path}
+          alt={movieList[2].title}
         />
         <Description>
           <Title>{movieList[2].title}</Title>

4dde8cb455b0e1f6f6997c5a93aadb276d7caf84
Rodrigo Russo <brweb0117@ambev.com.br>
2021-05-20 16:11:55 -0300

    Change Buttom sizes

--- src/components/element/Button/styles.ts
+++ src/components/element/Button/styles.ts
@@ -1,15 +1,22 @@
 import styled from "styled-components";
-import { StyleProps } from "./types";
+import { Breakpoints, Colors } from 'shared/enums';
+import { ContainerProps } from './types';
 
-export const Container = styled.button<StyleProps>`
-  height: ${(props) => (props.size === "default" ? "72px" : "60px")};
-  width: ${(props) => (props.size === "default" ? "360px" : "300px")};
+export const Container = styled.button<ContainerProps>`
+  width: 300px; //360;
+  height: 60px; // 72
   border-radius: 45px;
-  font-size: ${(props) => (props.size === "default" ? "24px" : "20px")};
+  font-size: 20px; //24
   color: ${(props) => props.color};
   background-color: ${(props) => props.backgroundColor};
   border: ${(props) =>
     props.outlined ? `3px solid ${props.borderColor}` : "none"};
   text-transform: uppercase;
   cursor: pointer;
+
+  @media screen and (min-width: ${Breakpoints.TABLET}) {
+    width: 360px;
+    height: 72px;
+    font-size: 24px;
+  }
 `;
--- src/components/element/Button/types/index.ts
+++ src/components/element/Button/types/index.ts
@@ -1,17 +1,15 @@
-export type StyleProps = {
-    outlined: boolean;
-    backgroundColor: string;
-    borderColor: string;
-    color: string;
-    size: "default" | "small";
-}
-
+import { Colors } from 'shared/enums';
 export type Props = {
     children?: string | JSX.Element;
     outlined: boolean;
-    background?: string;
-    borderColor?: string;
-    textColor?: string;
-    size: "default" | "small";
+    background?: Colors;
+    borderColor?: Colors;
+    textColor?: Colors;
     clickAction(): void;
+  }
+
+  export type ContainerProps = {
+    backgroundColor: Colors;
+    borderColor: Colors;
+    outlined: boolean;
   }

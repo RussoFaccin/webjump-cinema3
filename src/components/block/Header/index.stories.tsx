7ca87710e2a28d10652199e8a4b938e18ce0c332
Rodrigo Russo <brweb0117@ambev.com.br>
2021-05-24 12:33:57 -0300

    Header Component

---
+++ src/components/block/Header/index.stories.tsx
@@ -0,0 +1,29 @@
+import React from "react";
+import { Meta, Story } from "@storybook/react";
+import { BrowserRouter as Router } from "react-router-dom";
+import { Colors } from "shared/enums";
+import Header from "./";
+
+export default {
+  title: "Components/Header",
+  component: Header,
+  parameters: {
+    backgrounds: {
+      default: "primary",
+      values: [
+        { name: "primary", value: Colors.PRIMARY },
+        { name: "secondary", value: Colors.SECONDARY },
+      ],
+    },
+  },
+} as Meta;
+
+const Template: Story = (args) => {
+  return (
+    <Router>
+      <Header {...args} />
+    </Router>
+  );
+};
+
+export const Default = Template.bind({});

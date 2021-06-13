0397397f97c42032f0a0a8d368036f0fbe82423a
Rodrigo Russo <brweb0117@ambev.com.br>
2021-06-12 22:59:47 -0300

    First Implementation

--- src/pages/Profile/index.tsx
+++ src/pages/Profile/index.tsx
@@ -1,5 +1,9 @@
-import React from "react";
+import React, { useCallback, useEffect, useState } from "react";
 import { Footer, Header } from "components/block";
+import { FiLock, FiMail, FiUser } from "react-icons/fi";
+import { Colors } from "shared/enums";
+import { useAuth } from "contexts";
+import { FormType } from "./types";
 import {
   Button,
   Container,
@@ -8,36 +12,81 @@ import {
   FormInput,
   UserCircle,
 } from "./styles";
-import { FiLock, FiMail, FiUser } from "react-icons/fi";
-import { Colors } from "shared/enums";
 
 const Profile = () => {
+  const { user } = useAuth();
+
+  const [form, setForm] = useState<FormType>({
+    Nome: user ? user.name : "",
+    "E-mail": user ? user.email : "",
+    "Nova Senha": "",
+    "Confirmar Senha": "",
+  });
+
+  const [isLoading, setLoading] = useState(false);
+
+  const handleChange = useCallback(
+    (input) => {
+      setForm({
+        ...form,
+        [input.label]: input.value,
+      });
+    },
+    [form]
+  );
+
+  const handleSubmit = useCallback((evt) => {
+    evt.preventDefault();
+    evt.target.checkValidity()
+    setLoading(true);
+  }, []);
+
   return (
     <Container>
       <Header background={Colors.SECONDARY} />
       <Content>
         <UserCircle />
-        <Form>
+        <Form onSubmit={handleSubmit} noValidate>
           <FormInput
             icon={<FiUser />}
             label="Nome"
             type="text"
-            required
+            required={true}
             validationMessage="Campo obrigatório."
+            value={form["Nome"]}
+            actionInput={handleChange}
           />
           <FormInput
             icon={<FiMail />}
             label="E-mail"
             type="email"
-            required
+            required={true}
             validationMessage="Campo obrigatório. Preencha um e-mail válido."
+            value={form["E-mail"]}
+            actionInput={handleChange}
           />
-          <FormInput icon={<FiLock />} label="Nova Senha" type="password" />
-          <FormInput icon={<FiLock />} label="Confirmar Senha" type="password" />
+          <FormInput
+            icon={<FiLock />}
+            label="Nova Senha"
+            type="password"
+            required={true}
+            validationMessage="Campo obrigatório."
+            actionInput={handleChange}
+          />
+          <FormInput
+            icon={<FiLock />}
+            label="Confirmar Senha"
+            type="password"
+            required={true}
+            validationMessage="Campo obrigatório."
+            value={form["Confirmar Senha"]}
+            actionInput={handleChange}
+          />
           <Button
             outlined={true}
             background={Colors.SECONDARY}
             clickAction={() => false}
+            isLoading={isLoading}
           >
             ATUALIZAR PERFIL
           </Button>
